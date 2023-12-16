15f8ce5730315bcf09efe6a6d030d6c88861b246
NKLam <khoalamvn@gmail.com>
2023-12-16 11:47:10 +1100

    Add step 4

--- README.md
+++ README.md
@@ -20,8 +20,22 @@ In VsCode terminal, run `conda env list` to show the list of conda environment y
 
 Then run `conda activate BCI-Gazetrack` (or different name if you changed it). Run `conda env list` again to confirm, the * should be at the same line as BCI-Gazetrack.
 
+![Step 3 example img](example/step3.png "Step 3")
+
 Open the `step3.py` file to see if there are warnings about "cv2 can't resolve". You can open VsCode Command Pallete (Ctrl + Shift + P on Windows) and choose **Python: Select Interpreter**, then choose BCI-Gazetrack one.
 
 Run `python step3.py`. If your computer has a camera connected (hopefully), it will show you *a breathtaking sight*!
 
-You can read a bit in the code to understand the minimal code to get OpenCV running.
+**Press Q to exit**. You can read a bit in the code to understand the minimal code to get OpenCV running.
+
+#### Step 4: Mediapipe library
+
+[MediaPipe](https://github.com/google/mediapipe) is an open-source framework from Google for building pipelines to perform computer vision inference over arbitrary sensory data such as video or audio.
+
+We will use this library to map the facial landmarks of our face, therefore get the location of important features such as the eyes and irises.
+
+Run `python step4.py`, and it should also open your camera with extra stuff draw on your face. We mostly only care about the FACEMESH_TESSELATION (which is the web-like, grey thin lines draw on your face).
+
+![Step 4 example img](example/step4.png "Step 4")
+
+ Each or the vertices for this mesh called a landmark and has an id, which you can reference with `facemesh_landmark_ref` in `\res` folder. Zoom in real close and you will see the number.
---
+++ step4.py
@@ -0,0 +1,80 @@
+import cv2
+import mediapipe as mp
+from sys import platform
+
+CAM_WIDTH = 1280
+CAM_HEIGHT = 720
+MAX_PEOPLE = 1
+
+def main():
+    if platform == "win32":
+        cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    else:
+        cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, CAM_WIDTH)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, CAM_HEIGHT)
+    cam.set(cv2.CAP_PROP_FPS, 30)
+    cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+
+    # Setup facemesh setting
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    mp_face_mesh = mp.solutions.face_mesh
+    face_mesh = mp_face_mesh.FaceMesh(max_num_faces=MAX_PEOPLE, refine_landmarks=True, 
+        min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+    while True:
+        success, frame = cam.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
+
+        # To improve performance, optionally mark the frame as not writeable to
+        # pass by reference.
+        frame.flags.writeable = False
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = face_mesh.process(frame)
+
+        # Draw the face mesh annotations on the frame.
+        frame.flags.writeable = True
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                # Draw the mesh of your face with thin grey light
+                mp_drawing.draw_landmarks(
+                    image=frame,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_TESSELATION,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style(),
+                )
+
+                # Draw the line for the outline of your face with thick white line
+                mp_drawing.draw_landmarks(
+                    image=frame,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_CONTOURS,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style(),
+                )
+
+                # Draw your eyes
+                mp_drawing.draw_landmarks(
+                    image=frame,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_IRISES,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_iris_connections_style(),
+                )
+                
+        # Flip the image horizontally for a selfie-view display.
+        cv2.imshow("MediaPipe Face Mesh", cv2.flip(frame, 1))
+        if cv2.waitKey(1) & 0xFF == ord("q"):  
+            break
+
+    cv2.destroyAllWindows()
+    cam.release()
+
+if __name__ == '__main__':
+    main()
